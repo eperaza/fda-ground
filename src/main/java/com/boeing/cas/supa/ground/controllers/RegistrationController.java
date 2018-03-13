21c4648d337d834707bd2c05156cae299eaf9f4a
Mihir Shah <mihir.shah@boeing.com>
2018-03-13 15:46:41 -0700

    removing dependency

--- src/main/java/com/boeing/cas/supa/ground/controllers/RegistrationController.java
+++ src/main/java/com/boeing/cas/supa/ground/controllers/RegistrationController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.boeing.cas.supa.ground.helpers.AzureADClientHelper;
-import com.boeing.cas.supa.ground.pojos.AccessToken;
 import com.boeing.cas.supa.ground.pojos.Credential;
 import com.boeing.cas.supa.ground.pojos.Error;
 import com.boeing.cas.supa.ground.pojos.UserRegistration;
