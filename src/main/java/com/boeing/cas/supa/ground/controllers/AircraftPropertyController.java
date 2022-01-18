178af83045090e9c5a70140f5b1fcdeb35649282
Eduardo Peraza Acosta <eduardo.perazaacosta@boeing.com>
2022-01-18 15:05:10 -0800

    add logging to Registration Controller

--- src/main/java/com/boeing/cas/supa/ground/controllers/AircraftPropertyController.java
+++ src/main/java/com/boeing/cas/supa/ground/controllers/AircraftPropertyController.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.LastModified;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
@@ -87,14 +88,14 @@ public class AircraftPropertyController {
 
         //if lastUpdated is null or older than last modified then return the existing package
         if (lastUpdated != null && tspExists) {
-            logger.debug("DATE WAS PASSED IN!!!");
             Date lastModified = fileManagementService.getBlobLastModifiedTimeStamp(container, fileName);
+            logger.error("Date passed in is equal or newer than lastModified: " + ((lastUpdated.compareTo(lastModified) >= 0) ? "True" : "False" ));
             logger.debug("retrieved timestamp: " + lastModified.toString());
 
             // lastUpdated is newer or the same as the last time it was modified
             if (lastUpdated.compareTo(lastModified) >= 0) {
                 // do nothing, return
-                logger.debug("lastUpdated is newer than lastModified");
+                logger.error("Date passed in is equal or newer than lastModified: " + ((lastUpdated.compareTo(lastModified) >= 0) ? "True" : "False" ));
                 return new ResponseEntity<>(HttpStatus.OK);
             } else {
                 return aircraftPropertyService.getExistingTspPackage(authToken, container, fileName);
