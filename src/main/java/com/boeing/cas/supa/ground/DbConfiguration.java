a9977eb24a7ae4ca926a18a0ecb531cf124f302a
Eduardo Peraza Acosta <eduardo.perazaacosta@boeing.com>
2022-06-27 09:49:51 -0700

    bind tomcat pool properties to data source

--- src/main/java/com/boeing/cas/supa/ground/DbConfiguration.java
+++ src/main/java/com/boeing/cas/supa/ground/DbConfiguration.java
@@ -5,8 +5,10 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -29,7 +31,9 @@ public class DbConfiguration {
     @Value("${hibernate.hbm2ddl.auto}")
     private String hibernateHbm2ddlAuto;
     
+	@ConfigurationProperties(prefix = "spring.datasource.tomcat")
     @Bean
+	@Primary
 	public DataSource dataSource() {
 		DataSourceBuilder factory = DataSourceBuilder.create()
 				.url(keyVaultRetriever.getSecretByKey("SQLDatabaseUrl"))
